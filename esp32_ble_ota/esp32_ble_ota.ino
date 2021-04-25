e1ee94bfd10dcaa22ed4f440c00cc68974890ee8
Felix Biego <45796740+fbiego@users.noreply.github.com>
2021-04-25 13:20:35 +0300

    optimized transfer speed

--- esp32_ble_ota/esp32_ble_ota.ino
+++ esp32_ble_ota/esp32_ble_ota.ino
@@ -22,8 +22,10 @@
    SOFTWARE.
 */
 
+
 #include <Update.h>
 #include "FS.h"
+#include "FFat.h"
 #include "SPIFFS.h"
 #include <BLEDevice.h>
 #include <BLEUtils.h>
@@ -32,12 +34,26 @@
 
 #define BUILTINLED 2
 #define FORMAT_SPIFFS_IF_FAILED true
+#define FORMAT_FFAT_IF_FAILED true
+
+#define USE_SPIFFS  //comment to use FFat
+
+#ifdef USE_SPIFFS
+#define FLASH SPIFFS
+#else
+#define FLASH FFat
+#endif
+
+#define NORMAL_MODE   0   // normal
+#define UPDATE_MODE   1   // receiving firmware
+#define OTA_MODE      2   // installing firmware
 
 uint8_t major = 1;
-uint8_t minor = 4;
+uint8_t minor = 6;
 uint8_t ver[] = {0xFA, major, minor};  // code version
 
-uint8_t updater[16384]; // >= MainActivity.PART
+uint8_t updater[16384];
+uint8_t updater2[16384];
 
 #define SERVICE_UUID              "fb1e4001-54ae-4a28-9f74-dfccb248601d"
 #define CHARACTERISTIC_UUID_RX    "fb1e4002-54ae-4a28-9f74-dfccb248601d"
@@ -48,12 +64,15 @@ static BLECharacteristic* pCharacteristicRX;
 
 static bool deviceConnected = false;
 static int id = 0;
-static bool updating = false;
-static bool request = false;
+static bool writeFile = false;
+static int writeLen = 0;
+static int writeLen2 = 0;
+static bool current = true;
 static int parts = 0;
 static int next = 0;
 static int cur = 0;
 static int MTU = 0;
+static int MODE = NORMAL_MODE;
 
 static void rebootEspWithReason(String reason) {
   Serial.println(reason);
@@ -111,35 +130,44 @@ class MyCallbacks: public BLECharacteristicCallbacks {
         //        Serial.print(pCharacteristic->getUUID().toString().c_str());
         //        Serial.print(" of data length ");
         //        Serial.println(len);
-        Serial.print("RX  ");
-        for (int i = 0; i < len; i++) {
-          Serial.printf("%02X ", pData[i]);
-        }
-        Serial.println();
+        //        Serial.print("RX  ");
+        //        for (int i = 0; i < len; i++) {         // leave this commented
+        //          Serial.printf("%02X ", pData[i]);
+        //        }
+        //        Serial.println();
 
         if (pData[0] == 0xFB) {
           int pos = pData[1];
           for (int x = 0; x < len - 2; x++) {
-            updater[(pos * MTU) + x] = pData[x + 2];
+            if (current) {
+              updater[(pos * MTU) + x] = pData[x + 2];
+            } else {
+              updater2[(pos * MTU) + x] = pData[x + 2];
+            }
           }
 
         } else if  (pData[0] == 0xFC) {
-          int len = (pData[1] * 256) + pData[2];
-          cur = (pData[3] * 256) + pData[4];
-          writeBinary(SPIFFS, "/update.bin", updater, len);
-          if (cur < parts - 1) {
-            request = true;
+          if (current) {
+            writeLen = (pData[1] * 256) + pData[2];
+          } else {
+            writeLen2 = (pData[1] * 256) + pData[2];
           }
-        } else if (pData[0] == 0xFD) {
-          SPIFFS.format();
-        } else if  (pData[0] == 0xF0) {
+          current = !current;
+          cur = (pData[3] * 256) + pData[4];
 
+          writeFile = true;
+        } else if (pData[0] == 0xFD) {
+          if (FLASH.exists("/update.bin")) {
+            FLASH.remove("/update.bin");
+          }
         } else if  (pData[0] == 0xFE) {
-          rebootEspWithReason("Rebooting to start OTA update");
+          //rebootEspWithReason("Rebooting to start OTA update");
+
         } else if  (pData[0] == 0xFF) {
           parts = (pData[1] * 256) + pData[2];
           MTU = (pData[3] * 256) + pData[4];
-          updating = true;
+          MODE = UPDATE_MODE;
+
         }
 
 
@@ -147,23 +175,22 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 
     }
 
-    void writeBinary(fs::FS &fs, const char * path, uint8_t *dat, int len) {
-
-      //Serial.printf("Write binary file %s\r\n", path);
-
-      File file = fs.open(path, FILE_APPEND);
-
-      if (!file) {
-        Serial.println("- failed to open file for writing");
-        return;
-      }
-      file.write(dat, len);
-      file.close();
-    }
 
 };
 
+void writeBinary(fs::FS &fs, const char * path, uint8_t *dat, int len) {
 
+  //Serial.printf("Write binary file %s\r\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+
+  if (!file) {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  file.write(dat, len);
+  file.close();
+}
 
 void initBLE() {
   BLEDevice::init("ESP32 OTA");
@@ -195,12 +222,19 @@ void setup() {
   Serial.println("Starting BLE OTA sketch");
   pinMode(BUILTINLED, OUTPUT);
 
+#ifdef USE_SPIFFS
   if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
     Serial.println("SPIFFS Mount Failed");
     return;
   }
+#else
+  if (!FFat.begin()) {
+    Serial.println("FFat Mount Failed");
+    if (FORMAT_FFAT_IF_FAILED) FFat.format();
+    return;
+  }
+#endif
 
-  updateFromFS(SPIFFS);
 
   initBLE();
 
@@ -208,43 +242,66 @@ void setup() {
 
 void loop() {
 
-  if (deviceConnected) {
-    digitalWrite(BUILTINLED, HIGH);
+  switch (MODE) {
 
-    if (id < 512) {
-      if (id >= 510) {
-        pCharacteristicTX->setValue(ver, 3);
-        pCharacteristicTX->notify();
-        delay(100);
+    case NORMAL_MODE:
+      if (deviceConnected) {
+        digitalWrite(BUILTINLED, HIGH);
+
+        if (id < 512) {
+          if (id >= 510) {
+            pCharacteristicTX->setValue(ver, 3);
+            pCharacteristicTX->notify();
+            delay(100);
+          }
+          id++;
+        }
+      } else {
+        digitalWrite(BUILTINLED, LOW);
       }
-      id++;
-    }
-    if (updating) {
-      if (request) {
-        uint8_t rq[] = {0xF1, (cur + 1) / 256, (cur + 1) % 256};
-        pCharacteristicTX->setValue(rq, 3);
-        pCharacteristicTX->notify();
-        delay(100);
-        request = false;
-      }
+
+
+      break;
+
+    case UPDATE_MODE:
+
       if (cur + 1 == parts) { // received complete file
         uint8_t com[] = {0xF2, (cur + 1) / 256, (cur + 1) % 256};
         pCharacteristicTX->setValue(com, 3);
         pCharacteristicTX->notify();
-        delay(100);
-        updating = false;
+        delay(50);
+        MODE = OTA_MODE;
       }
-    }
-  } else {
-    digitalWrite(BUILTINLED, LOW);
+
+      if (writeFile) {
+        if (!current) {
+          writeBinary(FLASH, "/update.bin", updater, writeLen);
+        } else {
+          writeBinary(FLASH, "/update.bin", updater2, writeLen2);
+        }
+        writeFile = false;
+      }
+
+      break;
+
+    case OTA_MODE:
+      updateFromFS(FLASH);
+      break;
+
   }
 
 }
 
-
+void sendOtaResult(String result) {
+  pCharacteristicTX->setValue(result.c_str());
+  pCharacteristicTX->notify();
+  delay(200);
+}
 
 
 void performUpdate(Stream &updateSource, size_t updateSize) {
+  char s1 = 0x0F;
+  String result = String(s1);
   if (Update.begin(updateSize)) {
     size_t written = Update.writeStream(updateSource);
     if (written == updateSize) {
@@ -253,23 +310,33 @@ void performUpdate(Stream &updateSource, size_t updateSize) {
     else {
       Serial.println("Written only : " + String(written) + "/" + String(updateSize) + ". Retry?");
     }
+    result += "Written : " + String(written) + "/" + String(updateSize) + " [" + String((written / updateSize) * 100) + "%] \n";
     if (Update.end()) {
       Serial.println("OTA done!");
+      result += "OTA Done: ";
       if (Update.isFinished()) {
         Serial.println("Update successfully completed. Rebooting...");
+        result += "Success!\n";
       }
       else {
         Serial.println("Update not finished? Something went wrong!");
+        result += "Failed!\n";
       }
+
     }
     else {
       Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+      result += "Error #: " + String(Update.getError());
     }
-
   }
   else
   {
     Serial.println("Not enough space to begin OTA");
+    result += "Not enough space for OTA";
+  }
+  if (deviceConnected) {
+    sendOtaResult(result);
+    delay(5000);
   }
 }
 
